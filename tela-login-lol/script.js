248a01958912a23a50db87fd6aa859ed9d2c39e8
claudionetto <claudio.netto@sou.unifeob.edu.br>
2023-09-10 20:55:42 -0300

    Tela Login LoL

--- tela-login-lol/script.js
+++ tela-login-lol/script.js
@@ -1,5 +1,6 @@
 
-const inputs = document.querySelectorAll(".input");
+const inputs = document.querySelectorAll('.input');
+const button = document.querySelector('.login__button');
 
 const handleFocus = ({ target }) => {
     const span = target.previousElementSibling;
@@ -12,8 +13,19 @@ const handleFocusOut = ({ target }) => {
         const span = target.previousElementSibling;
         span.classList.remove('span-active');
     }
+}
 
+const handleChange = () => {
+    const [username, password] = inputs;
+
+    if (username.value && password.value.length >= 8){
+        button.removeAttribute('disabled')
+    } else {
+        button.setAttribute('disabled', '')
+    }
 }
 
 inputs.forEach((input) => input.addEventListener('focus', handleFocus))
 inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut))
+inputs.forEach((input) => input.addEventListener('input', handleChange))
+
